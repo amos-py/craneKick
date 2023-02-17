7f0143d874da979c545fdd0219df498629c08e2b
swagriftic <tofuroof@gmail.com>
2023-02-17 13:00:01 +0100

    oops

--- craneKick/Code/code.js
+++ craneKick/Code/code.js
@@ -74,7 +74,6 @@ let enemy = new Sprite( {
         y: 10
     }
 }); 
-player.draw();
 // funksjon som starter programmet og sørger for at der fortsatter til det stoppes.
 function animate() {
     window.requestAnimationFrame(animate);
