0f65c2285206c2fc4575c824eefe78a16dfa004a
swagriftic <tofuroof@gmail.com>
2023-04-12 13:03:12 +0200

    spriteshet

--- craneKick/Code/variables.js
+++ craneKick/Code/variables.js
@@ -31,19 +31,22 @@ let playerAnimationState = 4;
 let playerAnimationIndex = 0;
 let playerAnitmationIndexFloat = 0.0;
 let playerAnimation = [
-    [               0 ], // 0: Still
-    [   4,  5,  6,  7 ], // 1: Walking west
-    [   8,  9, 10, 11 ], // 2: Walking east
-    [  12, 13, 14, 15 ], // 3: Walking north
-    [   0,  1,  2,  3 ]  // 4: Walking south
+    [               0 ], // 0: Idle høyre
+    [              10 ], // 1: Idle Venstre
+    [      0, 1, 2, 3 ], // 2: Walk Høyre
+    [  10, 11, 12, 13 ], // 3: Walk venstre
+    [               20], // 4: Crouch
+    [30], // 5Jump
+    [40,41], // 6Punch
+    [50,51], // 7PunchDown
     ];
 
 let playerAnimationFPS = 20;
 
 
 // Player sprites
-let spriteSheetURL = "./Art/Characters/sprite1.png";
-let spriteSheetRows = 4;
-let spriteSheetColumns = 4;
+let spriteSheetURL = "./Art/Characters/spritesheetDrew.png";
+let spriteSheetRows = 10;
+let spriteSheetColumns = 10;
 
 
